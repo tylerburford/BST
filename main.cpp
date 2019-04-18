88ca33c4b37a53ca35a6b5c3918d535df616ee9d
tylerburford <32855628+tylerburford@users.noreply.github.com>
2019-04-18 12:43:17 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <BST312.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ int main() {
+    ifstream myfile;
+    myfile.open("C://Users//Tyler//CLionProjects//BinarySearchTree//BST//test.txt");
+    BST_312<string> *BST = new BST_312<string>(); //initialize an instance of BST
+    vector<string> test;    //vector to hold the string we will test the BST with
+    string word;
+    int i = 0;
+    while(i<11){               //get items from file
+        myfile >> word;         //input into string
+        test.push_back(word);   //push string into testing vector
+        i++;
+    }
+    for(i=0;i<test.size();i++){ //insert items into BST
+        word = test[i];
+        BST->insertItem(word);
+    }
+
+    //test
+    int numNodes = BST->countNodes();
+    vector<string> InOT = BST->inOrderTraversal();
+    vector<string> PreOT = BST->preOrderTraversal();
+    vector<string> PostOT = BST->postOrderTraversal();
+    /*
+    string isInTree1 = "series";
+    string isInTree2 = "the";
+    string isInTree3 = "this";
+    string isNotInTree = "lamp";
+    bool test1 = BST->isItemInTree(isInTree1);
+    bool test2 = BST->isItemInTree(isInTree2);
+    bool test3 = BST->isItemInTree(isInTree3);
+    bool test4 = BST->isItemInTree(isNotInTree);
+    */
+    bool isEmpty = BST->isEmpty();
+    bool isFull = BST->isFull();
+
+    BST_312<string> *copyTree = new BST_312<string>;
+
+}
